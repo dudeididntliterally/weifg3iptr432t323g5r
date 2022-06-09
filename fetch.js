64c10c1db1ebfa67f81f32ff485a29444a26f3a1
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-06-08 22:23:16 -0400

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,4 +1,3 @@
-fetch('https://www.gaming-style.com/RushTeam/RushTeamWebGL/Pages/GetKeyNewGet.php?GetInfo=oxMwlTJpdc3IIbSXN6XY9HGLh3R15P1ihGAfFO%2bX%2fUA%3d').then((res) => res.php())
-.then(response => {
-    console.log(response)
-}).catch(error => console.log(error));
+fetch('https://www.gaming-style.com/RushTeam/RushTeamWebGL/Pages/GetKeyNewGet.php?GetInfo=oxMwlTJpdc3IIbSXN6XY9HGLh3R15P1ihGAfFO%2bX%2fUA%3d')
+  .then(response => response())
+  .then(data => console.log(data));
