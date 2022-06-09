4039bf9adabee47d1cfa5382cdfa79d3a2dd18e4
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-06-08 22:27:29 -0400

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,3 +1,16 @@
-fetch('https://www.gaming-style.com/RushTeam/RushTeamWebGL/Pages/GetKeyNewGet.php?GetInfo=oxMwlTJpdc3IIbSXN6XY9HGLh3R15P1ihGAfFO%2bX%2fUA%3d')
-  .then(response => response())
-  .then(data => console.log(data));
+fetch("https://www.gaming-style.com/RushTeam/RushTeamWebGL/Pages/GetKeyNewGet.php?GetInfo=oxMwlTJpdc3IIbSXN6XY9HGLh3R15P1ihGAfFO%2bX%2fUA%3d", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "Referer": "https://www.gaming-style.com/RushTeam/RushTeamWebGL/index.php",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});
